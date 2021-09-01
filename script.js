2a58a673475c7c2f3b9577d3fa98f96e7d1074e7
ali-create <57019728+ali-create@users.noreply.github.com>
2021-09-01 17:28:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,50 @@ const price = document.querySelector(".price");
 const isSale = document.querySelector(".is--sale");
 const saleDiscount = document.querySelector(".sale--discount");
 const starRating = document.querySelector(".star--rating");
+const description = document.querySelector(".description");
+
+const overlay = document.querySelector(".screenOverlay");
+const modal = document.querySelector(".purchase--modal");
+const purchase = document.querySelector(".purchaseBtn");
+const ship = document.querySelector(".ship");
+const purchaseComplete = document.querySelector(".purchaseComplete");
 
 productName.textContent = urlData.productName + " ";
-price.textContent = " " + urlData.price + "$";
+price.textContent = " " + urlData.price + "";
 isSale.textContent = urlData.sale ? "Is on sale" : "Is not on sale";
 saleDiscount.textContent = "Sale " + urlData.saleDiscount * 100 + "%";
-starRating.textContent = "Average Review: " + urlData.starRating + " Stars";
+starRating.innerHTML = "Average Review: " + urlData.starRating;
+description.innerHTML = urlData.Description;
 
 document.querySelector(
   ".title"
-).innerHTML = `${urlData.productName} at ${urlData.price}$ - AmoBuy`;
+).innerHTML = `${urlData.productName} at ${urlData.price} - AmoBuy`;
 
 document.querySelector(".img").src = urlData.imgSRC;
 document.querySelector(".img").width = urlData.imgScale * 2;
+
+purchase.addEventListener("click", function () {
+  overlay.classList.toggle("overlay");
+  modal.classList.toggle("hidden");
+});
+document.querySelector(".exit").addEventListener("click", function () {
+  overlay.classList.toggle("overlay");
+  modal.classList.toggle("hidden");
+});
+ship.addEventListener("click", function () {
+  if (
+    document.querySelector(".input--email").validity.valid &&
+    document.querySelector(".input--email").value != ""
+  ) {
+    overlay.classList.toggle("overlay");
+    modal.classList.toggle("hidden");
+    setTimeout(() => {
+      overlay.classList.toggle("overlay");
+      purchaseComplete.classList.toggle("hidden");
+    }, 500);
+  }
+});
+document.querySelector(".ok").addEventListener("click", function () {
+  overlay.classList.toggle("overlay");
+  purchaseComplete.classList.toggle("hidden");
+});
